0090676333f953347fd4f30df147d39d6db22e98
Eric Payne <duncecapsoftware@gmail.com>
2018-09-05 08:42:40 -0700

    Why?
    Because, deep down, I love those jerks!
    Here ya go!
    Don't say I never gave you nothing!
    You're Welcome!

---
+++ assets/js/maps.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var uluru = {lat: 45.292, lng: -122.334};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
